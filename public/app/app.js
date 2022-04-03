63b2c6c6e2a46c69880ce2f91f21c41be4e8e74d
Gabriel da Silva <gabrieldasilvaprivado1@gmail.com>
2022-04-03 16:50:25 -0300

    :recycle: Change map and reduce to flatmap

--- public/app/app.js
+++ public/app/app.js
@@ -3,12 +3,8 @@ import { handleStatus, log } from './utils/promise-helpers.js';
 document.querySelector('#myButton').onclick = () =>
   fetch('http://localhost:3000/notas')
     .then(handleStatus)
-    .then((notas) =>
-      notas.reduce((array, nota) => array.concat(nota.itens), [])
-    )
-    .then(log)
+    .then((notas) => notas.flatMap((notas) => notas.itens))
     .then((itens) => itens.filter((item) => item.codigo == '2143'))
-    .then(log)
     .then((itens) => itens.reduce((total, item) => total + item.valor, 0))
-    .then(console.log)
+    .then(log)
     .catch(log);
