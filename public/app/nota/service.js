5ce8d2407c0160072f0d08bf961cf85f71d44255
Gabriel da Silva <gabrieldasilvaprivado1@gmail.com>
2022-04-04 01:18:26 -0300

    :recycle: isolates the sum of the items in the service

--- public/app/nota/service.js
+++ public/app/nota/service.js
@@ -1,6 +1,12 @@
-import { handleStatus } from '../utils/promise-helpers';
+import { handleStatus } from '../utils/promise-helpers.js';
 
 const API = 'http://localhost:3000/notas';
+
+const sumItems = (code) => (notas) =>
+  notas
+    .flatMap((nota) => nota.itens)
+    .filter((item) => item.codigo == code)
+    .reduce((total, item) => total + item.valor, 0);
 
 export const notasService = {
   listAll() {
@@ -10,5 +16,8 @@ export const notasService = {
         console.log(err);
         return Promise.reject('Dont possible get notas fiscais');
       });
+  },
+  sumItems(code) {
+    return this.listAll().then(sumItems(code));
   },
 };
